fe209bbbf8eecdbedd3381340d55607278aa97b2
narasimha322 <kasinanarasimha123@gmail.com>
2024-12-22 11:15:22 +0530

    commited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navar.css"; // Assuming your CSS styles are stored here
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav id="desktop-nav">
-      <div className="logo"><img src="./logo.png" alt="logo"/></div>
-      <div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>  {/* Add Home link */}
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      {/* Desktop Navigation */}
+      <nav id="desktop-nav">
+        <div className="logo">
+          <img src="./logo.png" alt="logo" />
+        </div>
+        <div>
+          <ul className="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Hamburger Menu for Mobile */}
+      <nav id="hamburger-nav">
+        <div className="logo">
+          <img src="./logo.png" alt="logo" />
+          <button className="hamburger-icon" onClick={toggleMenu}>
+            ☰
+          </button>
+        </div>
+        {isMenuOpen && (
+          <ul className="menu-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        )}
+      </nav>
+    </>
   );
 }
 
